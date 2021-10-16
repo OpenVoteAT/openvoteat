1dda94dfe5c62450a777e336405adc365095664e
unknown <christoph02.cw@gmail.com>
2021-10-16 17:35:02 +0200

    :bug: removed unique key from election.name

--- src/models.py
+++ src/models.py
@@ -9,12 +9,12 @@ class Election(Base):
     __tablename__ = "election"
 
     id                  = Column(Integer, primary_key=True, index=True)
-    name                = Column(String, unique=True)
+    name                = Column(String)
     from_date           = Column(DateTime)
     to_date             = Column(DateTime)
     num_of_votes        = Column(Integer, default=True)
     choices             = Column(JSON, default=True)
-    
+
     votes               = relationship("Vote")
 
 class Code(Base):
