0360fa4e79bba2bf7b0301b2c655a146c851eb1e
unknown <christoph02.cw@gmail.com>
2021-10-16 17:22:08 +0200

    :construction: added Vote model + implemented relationship between vote and election

--- src/models.py
+++ src/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime, JSON
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
 
 from database import Base
 
@@ -12,9 +14,21 @@ class Election(Base):
     to_date             = Column(DateTime)
     num_of_votes        = Column(Integer, default=True)
     choices             = Column(JSON, default=True)
+    
+    votes               = relationship("Vote")
 
 class Code(Base):
     __tablename__ = "code"
 
     id                  = Column(Integer, primary_key=True, index=True)
     code                = Column(String, unique=True)
+
+
+class Vote(Base):
+    __tablename__ = "vote"
+
+    id                  = Column(Integer, primary_key=True, index=True)
+    choice              = Column(Integer)
+    election_id         = Column(Integer, ForeignKey('election.id'))
+
+    election            = relationship("Election", back_populates="votes")
