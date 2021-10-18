f084e62bbb339516886b5cced3e31decab2b787c
unknown <christoph02.cw@gmail.com>
2021-10-18 12:48:13 +0200

    :construction: started implementation of choice checking

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,25 @@
 import random
 import string
 
+from sqlalchemy.orm import Session
+
+import schemas
+
 # Generates a random hash with random length between 18 and 22
 def generate_ranodm_hash():
     length = random.randint(18,22)
     options = string.ascii_letters + "0123456789"
     out_str = ''.join(random.choice(options) for i in range(length))
     return out_str
+
+#check if choice is valid^
+# structure:
+#   {
+#       name: points,
+#       name: points
+#   }
+#   
+def is_valid_choice(electionChoice,userChoice):
+
+
+    return True
